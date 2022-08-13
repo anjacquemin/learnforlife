44b5806d4626f94e3f84c813ae9e7a7cf63ae37c
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-08-13 02:38:05 +0200

    dynamic progress bar + timer + listenner disable after a click

--- app/javascript/controllers/quizz_answer_controller.js
+++ app/javascript/controllers/quizz_answer_controller.js
@@ -4,64 +4,54 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["goodAnswer", "userAnswer", "questionId", "questionCard0","questionCard1", "questionCard2", "questionCard3", "questionCard4"]
+  static targets = ["userAnswer", "questionId", "questionCard0","questionCard1", "questionCard2", "questionCard3", "questionCard4", "cardCountBar", "cardCountNumber", "answer1", "answer2", "seconds", "tens"]
 
 
   connect() {
     console.log("Hello from our first Stimulus controller")
+    window.seconds = 0;
+    window.tens = 0;
+    window.appendTens = this.tensTarget;
+    window.appendSeconds = this.secondsTarget;
+    var Interval
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
   }
 
   checkAnswer(event) {
     console.log("click")
-    // console.log(`question target`)
-    // console.log(this.questionTarget)
-    // console.log(this.questionTarget.dataset.answerId)
-    // console.log(`user answer target`)
 
     // To handle when a user click on the text of the answer and not the div
+    var corrected_target = event.target.hasAttribute("data-user-answer-id") ? event.target : event.target.parentElement
 
-    console.log(event.target)
-    console.log(event.target.parentElement)
-    var user_answer = ""
-    var text_has_been_clicked
-    // console.log(this.userAnswerTarget.dataset.userAnswerId)
-    // const user_answer = this.userAnswerTarget.dataset.userAnswerId
-    if (!(event.target.hasAttribute("data-user-answer-id"))) {
-      text_has_been_clicked = true
-      user_answer = event.target.parentElement.dataset.userAnswerId
-    }else {
-      user_answer = event.target.dataset.userAnswerId
-      console.log(`user answer : ` + user_answer)
-    }
+    //disable click on all answers
+    Array.from(corrected_target.parentElement.children).forEach(answer => answer.dataset.action = " ")
 
-    const good_answer = this.goodAnswerTarget.dataset.goodAnswerId
-    const question_id = this.goodAnswerTarget.dataset.questionId
-    const next_question_id = parseInt(question_id) + 1
+    //Enable to cancel click on the answer, so the purple color triggered by hovering disappear
+    corrected_target.style.pointerEvents = "none"
 
-    console.log(question_id)
-    console.log(next_question_id)
+    const user_answer = corrected_target.dataset.userAnswerId
+    const good_answer = corrected_target.parentElement.dataset.goodAnswerId
+    const question_id = corrected_target.parentElement.dataset.questionId
+    const question_number = parseInt(corrected_target.parentElement.dataset.questionNumber)
+    const next_question_number = parseInt(question_number) + 1
+    const total_questions = corrected_target.parentElement.dataset.totalQuestions
+
+
+    this.cardCountBarTarget.style.width = `${((question_number + 1)/total_questions)*100}%`
+    this.cardCountNumberTarget.innerHTML = `${(parseInt(question_number) + 1)}/${total_questions}`
+
     if (user_answer === good_answer){
-      // console.log(`bonne reponse`)
-      // console.log(this.userAnswerTarget)
-      if (text_has_been_clicked) {
-        event.target.parentElement.classList.add("green-background")
-      } else{
-        event.target.classList.add("green-background")
-      }
+        corrected_target.classList.add("green-background")
     } else {
-      if (text_has_been_clicked) {
-        event.target.parentElement.classList.add("red-background")
-      } else{
-        event.target.classList.add("red-background")
-      }
+        corrected_target.classList.add("red-background")
     }
 
     const self = this
 
-    delay(1000).then(() => {
-      // console.log('ran after 1 second1 passed')
-      eval(`self.questionCard${parseInt(question_id)}Target.classList.add("d-none")`)
-      eval(`self.questionCard${next_question_id}Target.classList.remove("d-none")`)
+    delay(2000).then(() => {
+      eval(`self.questionCard${question_number}Target.classList.add("d-none")`)
+      eval(`self.questionCard${next_question_number}Target.classList.remove("d-none")`)
     });
   }
 }
@@ -69,4 +59,30 @@ export default class extends Controller {
 
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+function startTimer() {
+  tens++;
+
+  if(tens <= 9){
+    // appendTens.innerHTML = "0" + tens;
+  }
+
+  if (tens > 9){
+    // appendTens.innerHTML = tens;
+
+  }
+
+  if (tens > 99) {
+    console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+
+  if (self.seconds > 9){
+    self.appendSeconds.innerHTML = seconds;
+  }
 }
