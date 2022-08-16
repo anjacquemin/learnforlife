35d06ce1c22096f71f6eff766eca36ab2e305012
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-08-16 18:43:21 +0200

    add dynamic value and layout for theme subtheme show

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,8 @@ class Category < ApplicationRecord
   has_many :category_progresses
   has_one_attached :photo
   has_many :quizzs, class_name: :Quizz
+  has_many :quizz_levels, through: :quizzs
+  has_many :question_answers, through: :quizzs
 
   validates :name, presence: true
 end
