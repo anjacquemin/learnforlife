bc19abea47f5f995a5e86e4e6402a470f7d5f3eb
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-09-01 16:55:14 +0200

    modal style review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     all_user_items_progress = user.send("#{key}_progresses")
     all_user_items_progress.find_by({key.to_sym => item})&.unlocked
   end
+
+  def seconds_to_minutes(seconds)
+    minutes = seconds / 60
+    if minutes != 0
+      seconds = seconds % (minutes * 60)
+      "#{minutes}:#{seconds}"
+    else
+      seconds
+    end
+  end
 end
