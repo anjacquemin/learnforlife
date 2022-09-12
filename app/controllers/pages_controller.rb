b73370e0afaee37f92b93dd3b1e8c4ed001b21d1
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-09-12 18:40:51 +0200

    homepage with device form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,27 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    filepath = "#{Rails.root}/db/seed_input/coordinates.csv"
-
-    @lat_long_countries = []
-
-    @question_answer = QuestionAnswer.all
-
-    require "csv"
-
-    index = 0
-
-    CSV.foreach(filepath, headers: :first_row) do |row|
-      # puts "#{row['NOM']} #{row['Affectation']} #{row['CAPITALE']}"
-      index += 1
-      @lat_long_countries << {
-        country: row["name"],
-        lat: row["latitude"],
-        long: row["longitude"],
-        map_index: "map#{index}"
-      }
-    end
-
-
+    @user = current_user
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,17 @@ module ApplicationHelper
       seconds
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
